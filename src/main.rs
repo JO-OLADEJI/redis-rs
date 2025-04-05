58e1cedd4e75f9cd8f0135c409fbb3b0976a0e54
Joshua Oladeji <dejijolaoluwa@gmail.com>
2025-04-05 10:27:12 +0100

    add a get and set command handler

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,42 @@
-use mini_redis::{client, Result};
-
-async fn greet() {
-    println!("tokio!");
-}
+use mini_redis::{Connection, Frame};
+use tokio::net::{TcpListener, TcpStream};
 
 #[tokio::main]
-async fn main() -> Result<()> {
-    let greeting = greet();
+async fn main() {
+    let redis_listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
-    print!("Hello, ");
-    greeting.await;
+    loop {
+        let (socket, _) = redis_listener.accept().await.unwrap();
 
-    Ok(())
+        tokio::spawn(async move {
+            process(socket).await;
+        });
+    }
+}
+
+async fn process(socket: TcpStream) {
+    use mini_redis::Command::{self, Get, Set};
+    use std::collections::HashMap;
+
+    let mut connection = Connection::new(socket);
+    let mut db: HashMap<String, Vec<u8>> = HashMap::new();
+
+    while let Some(frame) = connection.read_frame().await.unwrap() {
+        let res = match Command::from_frame(frame).unwrap() {
+            Set(cmd) => {
+                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                Frame::Simple("OK".to_string())
+            }
+            Get(cmd) => {
+                if let Some(value) = db.get(cmd.key()) {
+                    Frame::Bulk(value.clone().into())
+                } else {
+                    Frame::Null
+                }
+            }
+            cmd => panic!("unimplemented {:?}", cmd),
+        };
+
+        connection.write_frame(&res).await.unwrap();
+    }
 }
